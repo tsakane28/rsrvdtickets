bc50ecb9cd83ebf7b7b191391adcc1f9ab817b43
tsakane28 <wesleytsakane116@gmail.com>
2025-04-12 13:18:09 +0200

    a

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
 				{" "}
 				Powered by{" "}
 				<a
-					href='https://github.com/tsak'
+					href='https://github.com/tsaka'
 					target='_blank'
 					className='text-[#705a2f]'
 				>
