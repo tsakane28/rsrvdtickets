ff782cbc8e86e2b5cac00346e195e71c653b800e
tsakane28 <wesleytsakane116@gmail.com>
2025-04-12 16:38:11 +0200

    reg

--- pages/register.js
+++ pages/register.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import { HiMail } from "react-icons/hi";
@@ -7,127 +7,134 @@ import { firebaseCreateUser } from "../utils/util";
 import { useRouter } from "next/router";
 import ReCAPTCHA from "react-google-recaptcha";
 
-const register = () => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [cpassword, setCPassword] = useState("");
-	const router = useRouter();
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCPassword] = useState("");
+  const [recaptchaVerified, setRecaptchaVerified] = useState(false);
+  const router = useRouter();
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (password === cpassword) {
-			firebaseCreateUser(email, password, router);
-			setEmail("");
-			setPassword("");
-			setCPassword("");
-		}
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password === cpassword && recaptchaVerified) {
+      firebaseCreateUser(email, password, router);
+      setEmail("");
+      setPassword("");
+      setCPassword("");
+    } else if (!recaptchaVerified) {
+      alert("Please verify reCAPTCHA.");
+    }
+  };
 
-	return (
-		<div>
-			<Head>
-				<title>Register | RSRVD</title>
-				<meta
-					name='description'
-					content='An event ticketing system built with NextJS and Firebase'
-				/>
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<main className='w-full flex items-center justify-between min-h-[100vh]'>
-				<div className='md:w-[60%] w-full flex flex-col items-center justify-center min-h-[100vh] px-[30px] py-[30px] relative'>
-					<Link href='/'>
-						<h2 className='text-2xl font-medium mb-6'>Create an account</h2>
-					</Link>
-					<form
-						className='w-full flex flex-col justify-center'
-						onSubmit={handleSubmit}
-					>
-						<label htmlFor='email'>Email address</label>
-						<div className='w-full relative'>
-							<input
-								type='email'
-								name='email'
-								className='border px-10 py-2 mb-3 rounded-md w-full'
-								required
-								value={email}
-								onChange={(e) => setEmail(e.target.value)}
-							/>
-							<HiMail className=' absolute left-4 top-3 text-gray-300 text-xl' />
-						</div>
-						<label htmlFor='password'>Password</label>
-						<div className='w-full relative'>
-							<input
-								type='password'
-								name='password'
-								className='border px-10 py-2 mb-4 rounded-md w-full'
-								minLength={6}
-								required
-								value={password}
-								onChange={(e) => setPassword(e.target.value)}
-							/>
-							<AiTwotoneLock className=' absolute left-4 top-3 text-gray-300 text-xl' />
-							{password === cpassword && cpassword !== "" && (
-								<AiFillCheckCircle className=' absolute right-4 top-3 text-green-500 text-xl' />
-							)}
-						</div>
-						<label htmlFor='cpassword'>Confirm Password</label>
-						<div className='w-full relative'>
-							<input
-								type='password'
-								name='cpassword'
-								className='border px-10 py-2 mb-4 rounded-md w-full'
-								required
-								minLength={6}
-								value={cpassword}
-								onChange={(e) => setCPassword(e.target.value)}
-							/>
-							<AiTwotoneLock className=' absolute left-4 top-3 text-gray-300 text-xl' />
-							{password === cpassword && cpassword !== "" && (
-								<AiFillCheckCircle className=' absolute right-4 top-3 text-green-500 text-xl' />
-							)}
-						</div>
-						{password !== cpassword && (
-							<p className='text-red-500 mb-2'>Password does not match</p>
-						)}
-						<ReCAPTCHA
-						sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-						onChange={handleVerify}
-						/>
-						<button
-							type='submit'
-							className='bg-[#FFD95A] p-3 font-medium hover:bg-[#C07F00] hover:text-[#FFF8DE] mb-3 rounded-md'
-						>
-							REGISTER
-						</button>
-						<p className='text-center'>
-							Already have an account?{" "}
-							<Link href='/login' className='text-[#C07F00]'>
-								Sign in
-							</Link>
-						</p>
-					</form>
-					<div className='absolute bottom-5 left-5'>
-						<p className='opacity-50 text-sm'>
-							<Link href='/'>RSRVD</Link> &copy; Copyright{" "}
-							{new Date().getFullYear()}{" "}
-						</p>
-					</div>
-				</div>
-				<div className='register md:w-[40%] h-[100vh] relative'>
-					<div className='absolute bottom-5 right-5'>
-						<a
-							href='https://github.com/tsakane28'
-							target='_blank'
-							className='text-gray-100'
-						>
-							Built by RSRVD
-						</a>
-					</div>
-				</div>
-			</main>
-		</div>
-	);
+  const handleVerify = (value) => {
+    setRecaptchaVerified(value);
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Register | RSRVD</title>
+        <meta
+          name="description"
+          content="An event ticketing system built with NextJS and Firebase"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="w-full flex items-center justify-between min-h-[100vh]">
+        <div className="md:w-[60%] w-full flex flex-col items-center justify-center min-h-[100vh] px-[30px] py-[30px] relative">
+          <Link href="/">
+            <h2 className="text-2xl font-medium mb-6">Create an account</h2>
+          </Link>
+          <form
+            className="w-full flex flex-col justify-center"
+            onSubmit={handleSubmit}
+          >
+            <label htmlFor="email">Email address</label>
+            <div className="w-full relative">
+              <input
+                type="email"
+                name="email"
+                className="border px-10 py-2 mb-3 rounded-md w-full"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <HiMail className="absolute left-4 top-3 text-gray-300 text-xl" />
+            </div>
+            <label htmlFor="password">Password</label>
+            <div className="w-full relative">
+              <input
+                type="password"
+                name="password"
+                className="border px-10 py-2 mb-4 rounded-md w-full"
+                minLength={6}
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <AiTwotoneLock className="absolute left-4 top-3 text-gray-300 text-xl" />
+              {password === cpassword && cpassword !== "" && (
+                <AiFillCheckCircle className="absolute right-4 top-3 text-green-500 text-xl" />
+              )}
+            </div>
+            <label htmlFor="cpassword">Confirm Password</label>
+            <div className="w-full relative">
+              <input
+                type="password"
+                name="cpassword"
+                className="border px-10 py-2 mb-4 rounded-md w-full"
+                required
+                minLength={6}
+                value={cpassword}
+                onChange={(e) => setCPassword(e.target.value)}
+              />
+              <AiTwotoneLock className="absolute left-4 top-3 text-gray-300 text-xl" />
+              {password === cpassword && cpassword !== "" && (
+                <AiFillCheckCircle className="absolute right-4 top-3 text-green-500 text-xl" />
+              )}
+            </div>
+            {password !== cpassword && (
+              <p className="text-red-500 mb-2">Password does not match</p>
+            )}
+            <ReCAPTCHA
+              sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+              onChange={handleVerify}
+            />
+            <button
+              type="submit"
+              className="bg-[#FFD95A] p-3 font-medium hover:bg-[#C07F00] hover:text-[#FFF8DE] mb-3 rounded-md"
+            >
+              REGISTER
+            </button>
+            <p className="text-center">
+              Already have an account?{" "}
+              <Link href="/login" className="text-[#C07F00]">
+                Sign in
+              </Link>
+            </p>
+          </form>
+          <div className="absolute bottom-5 left-5">
+            <p className="opacity-50 text-sm">
+              <Link href="/">RSRVD</Link> &copy; Copyright{" "}
+              {new Date().getFullYear()}
+            </p>
+          </div>
+        </div>
+        <div className="register md:w-[40%] h-[100vh] relative">
+          <div className="absolute bottom-5 right-5">
+            <a
+              href="https://github.com/tsakane28"
+              target="_blank"
+              className="text-gray-100"
+            >
+              Built by RSRVD
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 };
 
-export default register;
+export default Register;
