1abc9d2036ae03d56c021b34d7d93dc4b85bffa9
tsakane28 <wesleytsakane116@gmail.com>
2025-04-12 16:59:54 +0200

    time

--- utils/qr.js
+++ utils/qr.js
@@ -1,14 +1,7 @@
-// utils/qr.js
 import QRCode from "qrcode";
 
-/**
- * Generates a QR code as a base64-encoded string.
- * @param {string} text - The text to encode in the QR code.
- * @returns {Promise<string>} - A promise that resolves to the base64-encoded QR code string.
- */
 export const generateQRCode = async (text) => {
   try {
-    // Generate the QR code as a base64 string
     const qrCode = await QRCode.toDataURL(text);
     return qrCode;
   } catch (error) {
