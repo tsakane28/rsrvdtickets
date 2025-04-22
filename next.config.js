7de5e512dfa2002df41768be0e3c7ceca4166f3b
tsakane28 <wesleytsakane116@gmail.com>
2025-04-22 10:22:12 +0200

    feat: update commit, paynow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	webpack: (config, { isServer }) => {
+		// Handle Node.js modules used by Paynow
+		if (!isServer) {
+			config.resolve.fallback = {
+				...config.resolve.fallback,
+				fs: false,
+				net: false,
+				tls: false,
+				crypto: require.resolve('crypto-browserify'),
+				stream: require.resolve('stream-browserify'),
+				http: require.resolve('stream-http'),
+				https: require.resolve('https-browserify'),
+				zlib: require.resolve('browserify-zlib'),
+				path: require.resolve('path-browserify'),
+				url: require.resolve('url'),
+				util: require.resolve('util/'),
+				buffer: require.resolve('buffer/'),
+			};
+			config.plugins.push(
+				new (require('webpack')).ProvidePlugin({
+					process: 'process/browser',
+					Buffer: ['buffer', 'Buffer'],
+				})
+			);
+		}
+		return config;
+	},
 };
 
 module.exports = nextConfig;
